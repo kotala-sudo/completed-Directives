700bf11c5e6a7152bda93f225111c476df703411
kotala-sudo <pkotala@yahoo.com>
2021-03-12 08:29:28 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# completed-directives
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/completed-directives)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  showOrHide: boolean = true;
+  count: number = 0;
+
+  log: number[] = [];
+
+  onClick() {
+    this.showOrHide = !this.showOrHide;
+    this.log.push(this.count++);
+  }
+}
